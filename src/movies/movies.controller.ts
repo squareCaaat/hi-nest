6bd4bbaf713a38d4dbe5e41f9426e1909cb85682
squareCaaat <140076739+squareCaaat@users.noreply.github.com>
2024-08-16 18:33:36 +0900

    Feat routing CRUD

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('movies')
-export class MoviesController {}
+@Controller('movies') // = RequestMapping
+export class MoviesController {
+  @Get()
+  getAll(): string {
+    return 'This will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') year: string) {
+    return `We are searching for a movie with title ${year}`;
+  }
+
+  @Get(':id')
+  getMovie(@Param('id') id: string) {
+    return `This will return one movie with id ${id}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return movieData;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This will delete a movie with id: ${id}`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateData) {
+    return {
+      updatedMovie: id,
+      ...updateData,
+    };
+  }
+}
